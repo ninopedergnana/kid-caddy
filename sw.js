75e1642c361ced0484aa2fdfb411bae9315af57b
ninopedergnana <pedergnana.nino@gmail.com>
2026-07-11 18:21:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stableford-caddy-v6";
+const CACHE = "stableford-caddy-v8";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
